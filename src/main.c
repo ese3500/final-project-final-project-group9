69cdec2ed5e2bc2e040d0536eb03bf343a858eab
Abir Hossain <101730174+abirh2@users.noreply.github.com>
2023-05-06 22:30:31 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,21 @@
-//#include <Arduino.h>
-//#include <avr/io.h>
-//#include <util/delay.h>
-//#define F_CPU 16000000UL
-//#define BAUD_RATE 9600
-//#define BAUD_PRESCALER (((F_CPU / (BAUD_RATE * 16UL))) - 1)
-//
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 16000000UL
+#define BAUD_RATE 9600
+#define BAUD_PRESCALER (((F_CPU / (BAUD_RATE * 16UL))) - 1)
+
+#define SERVO_PIN PB1
+#define SERVO_MIN 1000
+#define SERVO_MAX 2000
+
 //char String[25];
-//void dht11_start(void);
-//uint8_t dht11_check_response(void);
-//uint8_t dht11_read_byte(void);
-//
-/*-------------------UART Functions----------------------*/
+uint16_t pos = 1400;
+
+///*-------------------UART Functions----------------------*/
 //void UART_init(void) {
 //    /* Set Baud Rate*/
 //    UBRR0H = (unsigned char) (BAUD_PRESCALER>>8);
@@ -18,7 +23,7 @@
 //    // Enable receiver and transmitter
 //    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
 //
-//    /* Set Frame Format*/
+//   /* Set Frame Format*/
 //    UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); // 8 data bits
 //    UCSR0C |= (1<<USBS0); // 2 stop bits
 //}
@@ -36,156 +41,6 @@
 //        StringPtr++;
 //    }
 //}
-//
-/*-----------------------------------------*/
-//
-//void Initialize() {
-//    // Set PD2 as output
-//    DDRD |= (1 << PORTD2);
-//}
-//
-//void dht11_start(void)
-//{
-//    // Set the pin high for at least 18ms
-//    PORTD |= (1 << PORTD2);
-//    _delay_ms(20);
-//
-//    // Set the pin low for at least 20us
-//    PORTD &= ~(1 << PORTD2);
-//    _delay_us(20);
-//}
-//
-//uint8_t dht11_check_response(void)
-//{
-//    // Wait for the sensor to respond
-//    _delay_us(40);
-//
-//    // Check if the sensor is sending a low signal for at least 80us
-//    if (!(PIND & (1 << PIND2)))
-//    {
-//        _delay_us(80);
-//        sprintf(String, "hit low \n");
-//        UART_putstring(String);
-//
-//        // Check if the sensor has released the line (high signal for at least 80us)
-//        if (PIND & (1 << PIND2))
-//        {
-//            sprintf(String, "hit high \n");
-//            UART_putstring(String);
-//            return 1;
-//        }
-//    }
-//
-//    return 0;
-//}
-//
-//uint8_t dht11_read_byte(void)
-//{
-//    uint8_t byte = 0;
-//
-//    for (int i = 7; i >= 0; i--)
-//    {
-//        // Wait for the sensor to send a low signal (bit start)
-//        while (!(PIND & (1 << PORTD2)));
-//
-//        // Wait for the sensor to release the line (bit duration)
-//        _delay_us(30);
-//
-//        // If the line is still low, the bit is a 0; otherwise, it's a 1
-//        if (PIND & (1 << PORTD2))
-//        {
-//            byte |= (1 << i);
-//        }
-//
-//        // Wait for the bit to end
-//        while (PIND & (1 << PORTD2));
-//    }
-//
-//    return byte;
-//}
-//
-//int main(void) {
-//    Initialize();
-//    UART_init();
-//    while (1)
-//    {
-//        dht11_start();
-//        if (dht11_check_response())
-//        {
-//            sprintf(String, "hit check res");
-//            UART_putstring(String);
-//            uint8_t hum_int = dht11_read_byte();
-//            uint8_t hum_dec = dht11_read_byte();
-//            uint8_t temp_int = dht11_read_byte();
-//            uint8_t temp_dec = dht11_read_byte();
-//            uint8_t checksum = dht11_read_byte();
-//
-//            // Checksum verification
-//            if ((hum_int + hum_dec + temp_int + temp_dec) == checksum)
-//            {
-//                // Convert raw data to human-readable format
-//                float humidity = (float) hum_int + ((float)hum_dec / 10.0);
-//                float temperature = (float) temp_int + ((float)temp_dec / 10.0);
-//
-//                // Send data to serial
-//                sprintf(String, "hit send data");
-//                UART_putstring(String);
-//
-//                sprintf(String, "Humidity: %u\n", humidity);
-//                UART_putstring(String);
-//                sprintf(String, "Temperature: %u\n", temperature);
-//                UART_putstring(String);
-//            }
-//        }
-//
-//        // Wait for 2 seconds before taking another measurement
-//        _delay_ms(2000);
-//    }
-//}
-//
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <avr/interrupt.h>
-
-#define F_CPU 16000000UL
-#define BAUD_RATE 9600
-#define BAUD_PRESCALER (((F_CPU / (BAUD_RATE * 16UL))) - 1)
-
-#define SERVO_PIN PB1
-#define SERVO_MIN 1000
-#define SERVO_MAX 2000
-
-char String[25];
-//uint16_t pos = 0;
-
-/*-------------------UART Functions----------------------*/
-void UART_init(void) {
-    /* Set Baud Rate*/
-    UBRR0H = (unsigned char) (BAUD_PRESCALER>>8);
-    UBRR0L = (unsigned char) BAUD_PRESCALER;
-    // Enable receiver and transmitter
-    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-
-   /* Set Frame Format*/
-    UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); // 8 data bits
-    UCSR0C |= (1<<USBS0); // 2 stop bits
-}
-
-void UART_send(unsigned char data) {
-    // Wait for empty transmit buffer
-    while (!(UCSR0A & (1<<UDRE0)));
-    // Put data into buffer and send data
-    UDR0 = data;
-}
-
-void UART_putstring(char* StringPtr) {
-    while(*StringPtr != 0x00) {
-        UART_send(*StringPtr);
-        StringPtr++;
-    }
-}
 
 /*-----------------------------------------*/
 
@@ -249,6 +104,7 @@ void servo_init() {
 
     // Set the ICR1 register to the top value for 50Hz PWM (20000us period)
     ICR1 = 1250;
+
 }
 
 void servo_write_us(uint16_t us) {
@@ -260,17 +116,21 @@ void servo_write_us(uint16_t us) {
 //    OCR1B = OCR1A * 20 / 100;
 }
 
-uint16_t getPosition() {
-    uint16_t pos;
-    if (ADC >= 1000) {
-        pos = 2000;
+void getPosition() {
+//    uint16_t pos;
+    if (ADC >= 600 && pos != 1000) {
+//        _delay_us(200);
+        servo_write_us(950);
+        _delay_us(200);
+        pos = 1000;
 //    } else if (ADC >= 333 && ADC <= 666) {
 //        pos = 1500;
-    } else {
-        pos = ADC + 1000;
+    } else if (ADC < 600 && pos != 200) {
+//        _delay_us(200);
+        servo_write_us(200);
+        _delay_us(200);
+        pos = 200;
     }
-
-    return  pos;
 //    _delay_ms(200);
 //    sprintf(String, "ADC: %u\n", ADC);
 //    UART_putstring(String);
@@ -282,10 +142,11 @@ uint16_t getPosition() {
 
 int main() {
     Initialize();
-    UART_init();
+//    UART_init();
 
     // Initialize the servo
     servo_init();
+//    servo_write_us(1000);
 
 //    sprintf(String, "hello");
 //    UART_putstring(String);
@@ -293,11 +154,11 @@ int main() {
     // Move the servo back and forth
     while (1) {
 //        _delay_ms(1000);
+        getPosition();
 //        getPosition();
-        servo_write_us(getPosition());
 //        sprintf(String, "hello \n");
 //        UART_putstring(String);
-        _delay_ms(1000);
+//        _delay_ms(1000);
 
     }
 }
