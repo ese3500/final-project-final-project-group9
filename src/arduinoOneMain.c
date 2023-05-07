171dd634ad583bbe36cc2a7bf38323b91d09b4fa
Abir Hossain <101730174+abirh2@users.noreply.github.com>
2023-05-07 14:38:43 -0400

    Update arduinoOneMain.c

--- src/arduinoOneMain.c
+++ src/arduinoOneMain.c
@@ -12,37 +12,7 @@
 #define SERVO_MIN 1000
 #define SERVO_MAX 2000
 
-//char String[25];
 uint16_t pos = 1400;
-
-///*-------------------UART Functions----------------------*/
-//void UART_init(void) {
-//    /* Set Baud Rate*/
-//    UBRR0H = (unsigned char) (BAUD_PRESCALER>>8);
-//    UBRR0L = (unsigned char) BAUD_PRESCALER;
-//    // Enable receiver and transmitter
-//    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-//
-//   /* Set Frame Format*/
-//    UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); // 8 data bits
-//    UCSR0C |= (1<<USBS0); // 2 stop bits
-//}
-//
-//void UART_send(unsigned char data) {
-//    // Wait for empty transmit buffer
-//    while (!(UCSR0A & (1<<UDRE0)));
-//    // Put data into buffer and send data
-//    UDR0 = data;
-//}
-//
-//void UART_putstring(char* StringPtr) {
-//    while(*StringPtr != 0x00) {
-//        UART_send(*StringPtr);
-//        StringPtr++;
-//    }
-//}
-
-/*-----------------------------------------*/
 
 void Initialize() {
     cli();
@@ -113,52 +83,28 @@ void servo_write_us(uint16_t us) {
 
     // Write the compare match value to OCR1A
     OCR1A = cmp_match;
-//    OCR1B = OCR1A * 20 / 100;
 }
 
 void getPosition() {
-//    uint16_t pos;
     if (ADC >= 600 && pos != 1000) {
-//        _delay_us(200);
         servo_write_us(950);
         _delay_us(200);
         pos = 1000;
-//    } else if (ADC >= 333 && ADC <= 666) {
-//        pos = 1500;
     } else if (ADC < 600 && pos != 200) {
-//        _delay_us(200);
         servo_write_us(200);
         _delay_us(200);
         pos = 200;
     }
-//    _delay_ms(200);
-//    sprintf(String, "ADC: %u\n", ADC);
-//    UART_putstring(String);
-//    sprintf(String, "Position: %d\n", pos);
-//    UART_putstring(String);
-
-
 }
 
 int main() {
     Initialize();
-//    UART_init();
 
     // Initialize the servo
     servo_init();
-//    servo_write_us(1000);
-
-//    sprintf(String, "hello");
-//    UART_putstring(String);
 
     // Move the servo back and forth
     while (1) {
-//        _delay_ms(1000);
         getPosition();
-//        getPosition();
-//        sprintf(String, "hello \n");
-//        UART_putstring(String);
-//        _delay_ms(1000);
-
     }
 }
